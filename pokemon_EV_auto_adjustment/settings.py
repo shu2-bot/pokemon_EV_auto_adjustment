cca20c5dc8c1d73fc0a8dfdb92bae9f18554e460
shu2-lab <sek441815@gmail.com>
2023-04-03 11:59:51 +0900

    setting.pyのnginxへの対応

--- pokemon_EV_auto_adjustment/settings.py
+++ pokemon_EV_auto_adjustment/settings.py
@@ -146,6 +146,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = '/usr/share/nginx/html/static'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = '/usr/share/nginx/html/media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
